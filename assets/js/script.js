a35adc8cc72e2bc34c02771cc0bf8a0594639cc4
saifurrehman15 <saifkhandada72@gmail.com>
2025-03-18 19:14:44 +0500

    section completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,43 @@
 $(".slider-wrrp").slick({
   slidesToShow: 4,
-  slidesToScroll: 2,
   dots: false,
   infinite: true,
   arrows: true,
-  speed: 500,
+  speed: 8000,
   autoplay: true,
-  autoplayspeed: 2000,
+  autoplaySpeed: 0,
+  cssEase: "linear",
   nextArrow: ".bnner-slider-btn.next",
   prevArrow: ".bnner-slider-btn.pre",
+  responsive: [
+    {
+      breakpoint: 1440,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+  ],
 });
 
 $(".slider-wrrp-2").slick({
   slidesToShow: 4,
   dots: false,
-  rtl: false,
   infinite: true,
   arrows: true,
-  speed: 5000,
+  speed: 8000,
   autoplay: true,
-  autoplayspeed: 2000,
+  autoplaySpeed: 0,
   cssEase: "linear",
   nextArrow: ".bnner-slider-btn.next",
   prevArrow: ".bnner-slider-btn.pre",
+  responsive: [
+    {
+      breakpoint: 1440,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+  ],
 });
 
 const btns = document.querySelectorAll(".btn-area button");
@@ -30,6 +45,6 @@ const btns = document.querySelectorAll(".btn-area button");
 btns.forEach((btn) => {
   btn.addEventListener("click", () => {
     btns.forEach((elem) => elem.classList.remove("active"));
-    btn.classList.add("active")
+    btn.classList.add("active");
   });
 });
