42fb70aa0c344f417bc76b7c7233301ed2b55610
saifurrehman15 <saifkhandada72@gmail.com>
2025-03-19 04:04:11 +0500

    add brand section

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,6 +40,28 @@ $(".slider-wrrp-2").slick({
   ],
 });
 
+$(".brand-slider").slick({
+  slidesToShow: 6,
+  dots: false,
+  infinite: true,
+  arrows: true,
+  speed: 8000,
+  autoplay: true,
+  autoplaySpeed: 0,
+  cssEase: "linear",
+  nextArrow: ".bnner-slider-btn.next",
+  prevArrow: ".bnner-slider-btn.pre",
+  responsive: [
+    {
+      breakpoint: 1440,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+  ],
+});
+
+
 const btnTab1 = document.querySelectorAll(".btn-area button");
 const btnTab2 = document.querySelectorAll(".tab-area button");
 const btnTab3 = document.querySelectorAll(".tabs-btn-2 button");
